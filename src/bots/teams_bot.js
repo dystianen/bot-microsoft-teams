2798dba69265b47365f6ad0757ac59fc050fd511
Dystian En <distian.enyus@gmail.com>
2026-05-13 17:19:38 +0700

    fix: chat

--- src/bots/teams_bot.js
+++ src/bots/teams_bot.js
@@ -1694,7 +1694,7 @@ class TeamsBot {
         .first();
       const chatMarker = teamsPage
         .locator(
-          '[data-tid="chat-list-view"], [data-tid="app-bar-navigation-list"], #teams-app-container, [data-test-id="chat-list"], .teams-app-canvas'
+          '[data-tid="chat-list-view"], [data-tid="app-bar-navigation-list"], #teams-app-container, [data-test-id="chat-list"], .teams-app-canvas, div:has-text("Chat")'
         )
         .first();
       const teamsErrorMarker = teamsPage
